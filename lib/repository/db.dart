f2d6f0f0a91625d0181be67c0d6df94fefd39b54
Aloysious Benoy <aloysiousbenoy@gmail.com>
2021-06-28 01:43:19 +0530

    migrated project to null safety

--- lib/repository/db.dart
+++ lib/repository/db.dart
@@ -3,29 +3,32 @@ import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DB {
-  final db = Firestore.instance;
+  final db = FirebaseFirestore.instance;
 
   Stream<QuerySnapshot> init() {
     return db.collection('CRUD').snapshots();
   }
 
-  Future<String> createData(String name) async {
-    DocumentReference ref = await db.collection('CRUD').add({'name': '$name 😎', 'todo': randomTodo()});
-    print(ref.documentID);
-    return ref.documentID;
+  Future<String> createData(String? name) async {
+    DocumentReference ref = await db
+        .collection('CRUD')
+        .add({'name': '$name 😎', 'todo': randomTodo()});
+    print(ref.id);
+    return ref.id;
   }
 
-  void readData(String id) async {
-    DocumentSnapshot snapshot = await db.collection('CRUD').document(id).get();
-    print(snapshot.data['name']);
+  void readData(String? id) async {
+    DocumentSnapshot<Map<String, dynamic>> snapshot =
+        await db.collection('CRUD').doc(id).get();
+    print(snapshot.data()!['name']);
   }
 
   void updateData(DocumentSnapshot doc) async {
-    await db.collection('CRUD').document(doc.documentID).updateData({'todo': 'please 🤫'});
+    await db.collection('CRUD').doc(doc.id).update({'todo': 'please 🤫'});
   }
 
   void deleteData(DocumentSnapshot doc) async {
-    await db.collection('CRUD').document(doc.documentID).delete();
+    await db.collection('CRUD').doc(doc.id).delete();
   }
 
   // Should not be inside here but probably inside a todoObject
